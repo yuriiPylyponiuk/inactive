433b154da88713e7c7ac1b596adebb4232b1e190
Yurii Pylyponiuk <rutzi112@gmail.com>
2020-11-22 20:02:02 +0200

    new broken version

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const TIMER = 10000;
-const INTERVALTIME= 5000;
+const TIMEINMILISECOND = 1000;
+const INTERVALTIME= 1000;
 var timeoutId, intervalId; 
 
 //Start timer and clean body if img created first
@@ -8,7 +8,7 @@ function startTimer() {
   if(a){
     a.remove()
   }
-  timeoutId = window.setTimeout(doInactive, TIMER)
+  timeoutId = window.setTimeout(doInactive, TIMEINMILISECOND)
 }
 
 //Create img and change imgs
@@ -20,22 +20,42 @@ function doInactive() {
   let positionTop = window.innerHeight;
   const MINLEFT = 0;
   const MINTOP = 0;
+  let imgLink = `img/${randomNumber(1,7)}.jpeg`;
 
-  positionLeft = positionLeft-500;
-  positionTop = positionTop-500;
+  console.log(imgLink)
+
+  positionLeft = positionLeft-600;
+  positionTop = positionTop-600;
 
   div.classList.add('inactiveImg');
-  div.style.backgroundImage=`url(img/${randomNumber(1,7)}.jpeg)`;
   div.appendChild(img);
   div.style.left = `${randomNumber(MINLEFT,positionLeft)}px`;
   div.style.top = `${randomNumber(MINTOP,positionTop)}px`;
   body.appendChild(div);
-
+  
+  if(img.width>600 ){
+    img.classList.add('maxWidth')
+  }
+  if(img.height>600){
+    img.classList.add('maxHeight')
+  }
+  img.src= imgLink;
 
   intervalId = setInterval(() => {
+    imgLink = `img/${randomNumber(1,7)}.jpeg`
+    img.classList.remove('maxWidth', 'maxHeight')
     div.style.left = `${randomNumber(MINLEFT,positionLeft)}px`;
-    div.style.top = `${randomNumber(MINTOP,positionTop)}px`;;
-    div.style.backgroundImage=`url(img/${randomNumber(1,7)}.jpeg)`;
+    div.style.top = `${randomNumber(MINTOP,positionTop)}px`;
+    
+
+    if(img.width>600 ){
+      img.classList.add('maxWidth')
+    }
+    if(img.height>600){
+      img.classList.add('maxHeight')
+    }
+    
+    img.src= imgLink;
   }, INTERVALTIME);
 }
 
